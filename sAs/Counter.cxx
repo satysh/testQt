72108cc5f09dc54b2c7d80c4d485e760d89c168a
Ilyas <satyshevi@gmail.com>
2020-10-16 21:33:53 +0300

    removed trash

--- sAs/Counter.cxx
+++ sAs/Counter.cxx
@@ -1,5 +1,4 @@
 #include "Counter.h"
-#include <QDebug>
 
 Counter::Counter() : QObject()
                    , m_nValue(0)
@@ -10,7 +9,6 @@ void Counter::slotInc()
 {
     emit counterChanged(++m_nValue);
     if (m_nValue > 5) {
-        qDebug() << "Goodbye Suka!";
         emit goodbye();
     }
 }
