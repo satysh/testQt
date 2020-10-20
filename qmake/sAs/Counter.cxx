0a9fc9d784072998ade397c87a9c88b4aa74ba9b
satyshev <satyshev@jinr.ru>
2020-10-20 15:14:19 +0300

    add signals and slots example which is build by qmake

---
+++ qmake/sAs/Counter.cxx
@@ -0,0 +1,16 @@
+#include "Counter.h"
+
+Counter::Counter() : QObject()
+                   , m_nValue(0)
+{
+}
+
+void Counter::slotInc()
+{
+    emit counterChanged(++m_nValue);
+    if (m_nValue > 5) {
+        emit goodbye();
+    }
+}
+
+
---
+++ qmake/sAs/main.cpp
@@ -0,0 +1,30 @@
+#include <QtWidgets>
+#include "Counter.h"
+#include "Test.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QLabel lbl("0");
+    QPushButton cmd("ADD");
+    Counter counter;
+    Test test;
+    
+    lbl.show();
+    cmd.show();
+
+    QObject::connect(&cmd, SIGNAL(clicked()),
+                     &counter, SLOT(slotInc())
+                     );
+    QObject::connect(&cmd, SIGNAL(clicked()),
+    	             &test, SLOT(slotPrint())
+    	             );
+    QObject::connect(&counter, SIGNAL(counterChanged(int)),
+                     &lbl, SLOT(setNum(int))
+                     );
+    QObject::connect(&counter, SIGNAL(goodbye()),
+                     &app, SLOT(quit())
+                     );
+    return app.exec();
+}
